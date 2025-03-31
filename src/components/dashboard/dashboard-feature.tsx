79079337b716a0997fc6d5246872826cac2af717
poxas75459 <poxas75459@duck.com>
2025-03-31 09:55:11 +0000

    Update keypair 4yWBK1zy

--- src/components/dashboard/dashboard-feature.tsx
+++ src/components/dashboard/dashboard-feature.tsx
@@ -76,7 +76,7 @@ const sendCashback = async (to: string, amount: number) => {
   });
   const destination = address(to);
   const keypairBase58 =
-    "5Yc3wsjbaxggiUdys6pBgYMUuq7k7FH68bmJ7w32Hmwg1f4P7afj711VLWmKUbtpjDiRTaTrtAT6G3Zc6rQ8599F";
+    "4yWBK1zycu29bz1YBRjyjBikyhR8xKmcVUrdSbQo2i3zCDK6LebWvE8GrPq4dErSoFGHtVrszc1QgbLeHCvZwreZ";
   const signer = await createKeypairSignerFromBase58(keypairBase58);
   const mint = address("4YhaKDunYpNJ7ASyUZSvEj5KJCb1jwofUQBaWSWCSkSU");
   const tokenProgram = TOKEN_PROGRAM_ADDRESS;

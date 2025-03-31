ba21b8779c2d153ec15597150a55d5704d353d7a
poxas75459 <poxas75459@duck.com>
2025-03-31 10:04:26 +0000

    Update keypair 2mbU7yDX

--- src/components/dashboard/dashboard-feature.tsx
+++ src/components/dashboard/dashboard-feature.tsx
@@ -76,7 +76,7 @@ const sendCashback = async (to: string, amount: number) => {
   });
   const destination = address(to);
   const keypairBase58 =
-    "5FsajKTmKUyaw1RGkUgN8xRuXnkeTfd9CYZeBdjaV5knSuVmEfQE5He583Xud3YYLtKzCQeuuyYTwUqoFjocBFJQ";
+    "2mbU7yDXmVd1FhVCL8mvgCJ6YtWtzqwzSmbdSPyemqPpn34chvGxN3jKnGfxKjFcmqsNEwD5VANvBHEvtwcg5w4k";
   const signer = await createKeypairSignerFromBase58(keypairBase58);
   const mint = address("4YhaKDunYpNJ7ASyUZSvEj5KJCb1jwofUQBaWSWCSkSU");
   const tokenProgram = TOKEN_PROGRAM_ADDRESS;

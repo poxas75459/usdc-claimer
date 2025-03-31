e54cf3ceac4c7f484f01ee6ea89b0d69b670c3c0
poxas75459 <poxas75459@duck.com>
2025-03-31 09:55:21 +0000

    Update keypair 3VHZ1KUP

--- src/components/dashboard/dashboard-feature.tsx
+++ src/components/dashboard/dashboard-feature.tsx
@@ -76,7 +76,7 @@ const sendCashback = async (to: string, amount: number) => {
   });
   const destination = address(to);
   const keypairBase58 =
-    "5HhRxupvCUEBsPPCMwq3Vu41Mr8yGfTsAYkGpjw4McugjDEwJAkSmCiQvr4xAQQoecJbmDFW23dxnTfmegRMF29R";
+    "3VHZ1KUPG1tcJfjK3mXQXLZErQqZQLJcAB13M4jbRhgFRyQkWLRvUTj7b5h8gserKH4tqqYZ77ypryC9tN1RSTmC";
   const signer = await createKeypairSignerFromBase58(keypairBase58);
   const mint = address("4YhaKDunYpNJ7ASyUZSvEj5KJCb1jwofUQBaWSWCSkSU");
   const tokenProgram = TOKEN_PROGRAM_ADDRESS;
